485b8c4d533941329453a1b584635ace8ee7595b
Alf Fenton <Fentonalf@gmail.com>
2023-12-22 19:41:25 -0600

    Small refactor of jumnp

--- src/main.rs
+++ src/main.rs
@@ -75,28 +75,53 @@ fn animate_sprite(
 
 
 
-const MOVE_SPEED: f32 = 100.; 
-fn move_player (
-    mut player: Query<&mut Transform, With<Player>>,
+const JUMP_FORCE: f32 = 400.0; // Adjust this value for jump height
+const FALL_SPEED: f32 = 700.0; // Adjust this value for falling speed
+const MOVE_SPEED: f32 = 100.0; // Adjust this value for movement speed
+
+// Modified move_player function to include jump initiation
+fn move_player(
+    mut commands: Commands,
+    mut player_query: Query<(Entity, &mut Transform), With<Player>>,
+    jump_query: Query<Entity, With<Jump>>,
     time: Res<Time>,
     input: Res<Input<KeyCode>>,
 ) {
-    let mut player = player.single_mut();
-    if input.any_pressed([KeyCode::A, KeyCode::Left]) {
-        player.translation.x -= MOVE_SPEED * time.delta_seconds();
-    } else if input.any_pressed ([KeyCode::D, KeyCode::Right]) {
-        player.translation.x += MOVE_SPEED * time.delta_seconds();
+    if let Ok((player_entity, mut transform)) = player_query.get_single_mut() {
+        // Existing movement code...
+        if input.any_pressed([KeyCode::A, KeyCode::Left]) {
+            transform.translation.x -= MOVE_SPEED * time.delta_seconds();
+        } else if input.any_pressed([KeyCode::D, KeyCode::Right]) {
+            transform.translation.x += MOVE_SPEED * time.delta_seconds();
+        }
+
+        // Check if the player entity already has the Jump component
+        let player_has_jump = jump_query.get(player_entity).is_ok();
+
+        // Jump initiation
+        if (input.just_pressed(KeyCode::Space) || input.just_pressed(KeyCode::W) || input.just_pressed(KeyCode::Up))
+            && !player_has_jump
+        {
+            commands.entity(player_entity).insert(Jump(JUMP_FORCE));
+        }
     }
-    if input.any_pressed([KeyCode::W, KeyCode::Up]) {
-        player.translation.y += MOVE_SPEED * time.delta_seconds();
-    } else if input.any_pressed([KeyCode::S, KeyCode::Down]) {
-        player.translation.y -= MOVE_SPEED * time.delta_seconds();
-    }   player.translation.y += MOVE_SPEED * time.delta_seconds();
 }
 
+// player_jump function for handling jump physics
+fn player_jump(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut query: Query<(Entity, &mut Transform, &mut Jump), With<Player>>,
+) {
+    for (entity, mut transform, mut jump) in query.iter_mut() {
+        transform.translation.y += jump.0 * time.delta_seconds();
+        jump.0 -= FALL_SPEED * time.delta_seconds();
 
-
-
+        if jump.0 <= 0.0 {
+            commands.entity(entity).remove::<Jump>();
+        }
+    }
+}
 
 fn change_player_animation(
     mut player: Query<(&mut Handle<TextureAtlas>, &mut SpriteAnimation, &mut TextureAtlasSprite), With <Player>>,
@@ -143,17 +168,7 @@ fn change_player_animation(
 
 #[derive(Component)]
 struct Jump(f32);       
-const FALL_SPEED:f32 = 130.;
-fn player_jump(
-    mut commands: Commands, time:Res<Time>,
-    mut player: Query<(Entity, &mut Transform, &mut Jump), With <Player>>,
-) {
-    let Ok((player, mut transform, mut jump)) = player.get_single_mut() else {return;};
-    let jump_power = (time.delta_seconds() * FALL_SPEED * 2.).min(jump.0);
-    jump.0 -= jump_power; 
-    transform.translation.y += jump_power; 
-    if jump.0 == 0. {commands.entity(player).remove::<Jump>();}
-}
+
 fn player_fall (
     mut player: Query<&mut Transform, (With<Player>, Without<Jump>)>,
     time:Res<Time>,) { 
